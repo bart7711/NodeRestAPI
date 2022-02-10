4c21a519feda46b12dd0346d0df82c0912c8dc80
bart7711 <bart0594@stud.kea.dk>
2022-02-10 10:40:09 +0100

    test/added functionality

--- app.js
+++ app.js
@@ -2,8 +2,37 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 let id = 0;
+const defultRout = "/movie";
 const movieArray = [];
 
-function generateId(){
-    return ++id;
+function generateId() {
+  return ++id;
 }
+
+function getMovieByID(id) {
+  return movieArray.find((movie) => {
+    return movie.id === parseInt(id);
+  });
+}
+
+app.get(defultRout, (req, res) => {
+  res.send(movieArray);
+});
+
+app.get(defultRout + "/:id", (req, res) => {
+    const movie = getMovieByID(req.params.id);
+  res.send(getMovieByID(req.params.id))
+});
+
+app.post(defultRout, (req, res) => {
+  const movie = req.body;
+  movie.id = generateId();
+  movieArray.push(movie);
+  res.send(movie);
+});
+
+app.delete(defultRout + "/:id", (req, res) => {
+    const index = getMovieByID
+});
+
+app.listen(8080);
